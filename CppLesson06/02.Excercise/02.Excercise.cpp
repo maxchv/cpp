cd5c787ef4d3a6f67152dbbc2e520f6d1db5ae8d
maxchv <maxshaptala@gmail.com>
2015-09-21 21:18:10 +0300

    Repair errors

--- CppLesson06/02.Excercise/02.Excercise.cpp
+++ CppLesson06/02.Excercise/02.Excercise.cpp
@@ -13,7 +13,7 @@ class Date
 
 	bool is_leap_year(int year)
 	{
-		if ((year % 400 == 0) || (year % 4 == 0) && year % 100)
+		if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
 		{
 			return true;
 		}
@@ -35,17 +35,17 @@ class Date
 		{
 			tmp += d[i];
 		}
-		if (is_leap_year(year))
+		if (month >= 2 && is_leap_year(year))
 		{
 			tmp += 1;
 		}
 
 		return tmp + day;
 	}
-	int get_days()
+	int get_days(int from)
 	{
 		int all_days = 0;
-		for (int i = 0; i < year - 1; i++)
+		for (int i = from; i < year; i++)
 		{
 			all_days += days_in_year(year);
 		}
@@ -57,13 +57,14 @@ public:
 	Date(int d, int m, int y) : day(d), month(m), year(y){}
 	int operator-(Date &right)
 	{
-		return get_days() - right.get_days();
+		int first = get_days(right.year);
+		int last = right.get_days(right.year);
+		return first - last;
 	}
-
 };
 int _tmain(int argc, _TCHAR* argv[])
 {
-	Date d1(1, 1, 2015), d2(1, 1, 2016);
+	Date d1(1, 1, 1990), d2(1, 12, 2016);
 	cout << d2 - d1 << endl;
 	return 0;
 }
