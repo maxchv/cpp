b95b81c37978adc5ddf19453bbcdd7344151dac7
maxchv <maxshaptala@gmail.com>
2015-10-12 16:20:07 +0300

    Added share_ptr

--- CppLesson12/05.StandartSmartPtr/05.StandartSmartPtr.cpp
+++ CppLesson12/05.StandartSmartPtr/05.StandartSmartPtr.cpp
@@ -16,13 +16,18 @@ void ex01()
 	cout << *pj << endl;
 	cout << pi.get() << " " << pj.get() << endl;
 	// cout << *pi << endl; ошибка на этапе выполнения т.к. pi = 0
+	cout << pi.get() << endl;  // адрес исходных данных
+	int* a = pi.release();
+	cout << pi.get() << " a: " << a << endl;
+	pi.reset(new int(15));
+	cout << pi.get() << endl;	
 }
 
 void ex02()
 {
 	unique_ptr<int> pi(new int(10));
 	cout << *pi << endl;
-
+	
 	unique_ptr<int> pj;
 	//pj = pi;
 	pj = move(pi); // перемещение данных из pi в pj
@@ -30,10 +35,21 @@ void ex02()
 	cout << pi.get() << " " << pj.get() << endl;
 }
 
+void ex03()
+{
+	shared_ptr<int> sh(new int(100));
+	shared_ptr<int> sh2 = sh;
+	cout << *sh << endl;
+	cout << *sh2 << endl;
+	cout << sh.use_count() << endl;
+}
+
 int main()
 {
 	ex01();
 	ex02();
+	ex03();
+
     return 0;
 }
 
